757e79bac6a88067ea2fd2a39c6ceb48103bd053
zxy <zxy@qq.com>
2020-03-18 17:21:08 +0800

    commit experiment layout

---
+++ LayoutExperiment/app/src/main/java/com/zxy/layoutexperiment/LinearLayoutActivity.java
@@ -0,0 +1,17 @@
+package com.zxy.layoutexperiment;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author 98578
+ */
+public class LinearLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+    }
+}
---
+++ LayoutExperiment/app/src/main/java/com/zxy/layoutexperiment/MainActivity.java
@@ -0,0 +1,56 @@
+package com.zxy.layoutexperiment;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btn_linearlayout,btn_relativelayout,btn_tablelayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    /**
+     * 初始化按钮
+     */
+    private void initView(){
+        btn_linearlayout = findViewById(R.id.linearly);
+        btn_relativelayout = findViewById(R.id.relatively);
+        btn_tablelayout = findViewById(R.id.tableau);
+
+        btn_linearlayout.setOnClickListener(this);
+        btn_relativelayout.setOnClickListener(this);
+        btn_tablelayout.setOnClickListener(this);
+    }
+
+    /**
+     * 方法单击事件
+     * @param v
+     */
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()){
+            case R.id.linearly:
+                intent = new Intent(MainActivity.this, LinearLayoutActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.relatively:
+                intent = new Intent(MainActivity.this, RelativeLayoutActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.tableau:
+                intent = new Intent(MainActivity.this, TableLayoutActivity.class);
+                startActivity(intent);
+                break;
+            default:break;
+        }
+    }
+}
